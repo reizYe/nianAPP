4d583cbc778e39483775a568c94bf107dd89866c
reiz <1359677939@qq.com>
2017-08-07 14:53:47 +0800

    修改定本

--- js/denglu.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("myApp",[]);
-    app.controller("myCtrl",function ($scope) {
-        var user = "reiz";
-        var password = "123456";
-    })
--- js/nian.js
+++ js/nian.js
@@ -8,6 +8,21 @@ app.config( function($stateProvider, $urlRouterProvider) {
             controller:'basicCtrl',
             templateUrl:'views/basic.html'
         })
+        .state('basic/basiccollect',{
+            url:'/basic/basiccollect',
+            controller:'basiccollectCtrl',
+            templateUrl:'views/basic/basiccollect.html'
+        })
+        .state('basic/basicsetting',{
+            url:'/basic/basicsetting',
+            controller:'basicsettingCtrl',
+            templateUrl:'views/basic/basicsetting.html'
+        })
+        .state('basic/basicmoney',{
+            url:'/basic/basicmoney',
+            controller:'basicmoneyCtrl',
+            templateUrl:'views/basic/basicmoney.html'
+        })
         // .state('basic/basicdaily',{
         //     url:'/basic/basicdaily',
         //     controller:'basicdailyCtrl',
@@ -18,23 +33,42 @@ app.config( function($stateProvider, $urlRouterProvider) {
             controller:'focusCtrl',
             templateUrl:'views/focus/focus.html'
         })
+        .state('focus/focusactive',{
+            url:'/focus/focusactive',
+            controller:'focusactiveCtrl',
+            templateUrl:'views/focus/focusactive.html'
+        })
         .state('find',{
             url:'/find',
             controller:'findCtrl',
             templateUrl:'views/find.html'
         })
         .state('find/findmore',{
-        url:'/find/findmore',
-        controller:'findmoreCtrl',
-        templateUrl:'views/find/findmore.html'
-    })
+            url:'/find/findmore',
+            controller:'findmoreCtrl',
+            templateUrl:'views/find/findmore.html'
+        })
+        .state('find/findnew',{
+            url:'/find/findnew',
+            controller:'findnewCtrl',
+            templateUrl:'views/find/findnew.html'
+        })
         .state('add',{
             url:'/add',
+            controller:'addCtrl',
             templateUrl:'views/add.html'
         })
         .state('message',{
             url:'/message',
+            controller:'messageCtrl',
             templateUrl:'views/message.html'
         })
+        .state('message/messagefocus',{
+            url:'/message/messagefocus',
+            controller:'messagefocusCtrl',
+            templateUrl:'views/message/messagefocus.html'
+        })
 });
+
+
 
